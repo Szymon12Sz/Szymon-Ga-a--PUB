d307283ea668b6866376e4b0545c59a8b158fb4c
Szymon Gałaś <szymon.galas@microsoft.wsei.edu.pl>
2022-03-30 19:23:44 +0200

    commit end

--- projekt1/index.js
+++ projekt1/index.js
@@ -4,3 +4,23 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 app.listen(4000);
+app.get('/:operation/:num1/:num2', function (req, res) {
+    var operation = (req.params.operation);
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    if (operation == 'dodaj') {
+        res.send((num1 + num2).toString());
+    }
+    else if (operation == 'odejmij') {
+        res.send((num1 - num2).toString());
+    }
+    else if (operation == 'pomnoz') {
+        res.send((num1 * num2).toString());
+    }
+    else if (operation == 'podziel') {
+        res.send((num1 / num2).toString());
+    }
+    else {
+        res.send('Podaj prawidlowa czynnosc');
+    }
+});
--- projekt1/index.ts
+++ projekt1/index.ts
@@ -1,6 +1,34 @@
 const express = require('express')  
-const app = express()  
+const app = express() 
 app.get('/', function (req, res) {  
   res.send('Hello World')  
+})   
+app.listen(4000) 
+app.get('/:operation/:num1/:num2', function (req, res) 
+{  
+  let operation = (req.params.operation);
+  let num1 = parseInt(req.params.num1);
+  let num2 = parseInt(req.params.num2);
+
+  if (operation == 'dodaj')
+  {
+    res.send((num1+num2).toString());
+  }
+  else if(operation == 'odejmij')
+  {
+    res.send((num1-num2).toString());
+  }
+  else if(operation == 'pomnoz')
+  {
+    res.send((num1*num2).toString());
+  }
+  else if(operation == 'podziel')
+  {
+    res.send((num1/num2).toString());
+  }
+  else
+  {
+    res.send('Podaj prawidlowa czynnosc')
+  }
 })  
-app.listen(4000)  
+ 
